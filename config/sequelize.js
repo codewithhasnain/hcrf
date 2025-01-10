d1aa65081ceea88f7add9eed5c602eae6589f386
codewithhasnain <163335665+codewithhasnain@users.noreply.github.com>
2025-01-10 05:42:27 +0500

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,11 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-// Initialize Sequelize
-const sequelize = new Sequelize('hcrf', 'root', '', { 
-    host: 'localhost',
+const sequelize = new Sequelize('defaultdb', 'avnadmin', 'AVNS_f9fjsOaNRKY6S-BNsRj', {
+    host: 'mysql-2b175d8d-kumailnaqvi382-5c70.c.aivencloud.com',
+    port: 22768,
     dialect: 'mysql',
-});
-
+    ssl: {
+      rejectUnauthorized: true,
+    },
+    dialectOptions: {
+      connectTimeout: 60000, // Set timeout to 60 seconds
+    },
+  });
+  
 // Test the connection
 (async () => {
     try {
